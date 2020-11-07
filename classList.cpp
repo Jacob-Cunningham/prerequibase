a19db0f2175e84633fabcbb65e087bab1dd1e92a
Jacob-Cunningham <jcunn008@ucr.edu>
2020-11-07 11:17:51 -0800

    started splitLine function

--- classList.cpp
+++ classList.cpp
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+vector<string> splitLine(string inputLine) {
+    vector<string> tempVector;
+    string tempString = inputLine;
+    int findIndex = 0;
+
+    while(tempString.find(',') != string::npos) {
+        findIndex = tempString.find(',', findIndex) - 1;
+        tempVector.push_back(tempString.substr(0, findIndex))
+        tempString.replace(0, findIndex + 1);
+    }
+
+}
+
 int main () {
     string currentClass;
     ifstream classList;
