cd43e983278870214a16f4965ff62dffea0faaae
Jacob Cunningham <jcunn008@ucr.edu>
2020-11-07 10:22:47 -0800

    started project

---
+++ classList.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    string currentClass;
+    ifstream classList;
+    classList.open("listOfClasses", ios::in | ios::app);
+    getline(classList, currentClass);
+
+    while (currentClass != "end") {
+
+    }
+    
+
+
+
+   return 0;
+
+    return 0;
+}
